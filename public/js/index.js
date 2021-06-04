2ce48bd91893bf9d80ff1e4d568f6691f43f8c1a
yl301114 <yl301114@gmail.com>
2021-06-05 04:17:42 +0800

    0605-2

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,12 @@
+// check box
+$(".check").click(function(){
+    if($(this).hasClass("active")){
+        $(this).removeClass("active");
+    }else {
+        $(this).addClass("active");
+    }
+})
+
 // buy num
 $(".minus").click(function () {
 	var buyNum = $(this).parents(".mb").children("input").val();
@@ -21,3 +30,27 @@ $(".plus").click(function () {
 	}
 	$(this).parents(".mb").children("input").val(clickNum);
 });
+
+// product feature
+$(".f-title").click(function(){
+    if($(this).hasClass("active")){
+        $(this).removeClass("active");
+        $(".f-content").slideUp(300);
+    }else {
+        $(this).addClass("active");
+        $(".f-content").slideDown(300);
+    }
+})
+
+// join member
+$(".tab").children("button").click(function(){
+    $(".tab").children("button").removeClass("active");
+    $(this).addClass("active");
+    if($(this).hasClass("login")){
+        $(".member-form.login").show();
+        $(".member-form.signup").hide();
+    }else if($(this).hasClass("signup")){
+        $(".member-form.login").hide();
+        $(".member-form.signup").show();
+    }
+})
